21748b7b2c0b7d6c749be9e3344b9e6d5333eee4
Alireza <real7alireza@gmail.com>
2021-01-14 03:07:33 +0330

    horizontal line

--- src/Components/Timer/TimerControl/TimerControl.js
+++ src/Components/Timer/TimerControl/TimerControl.js
@@ -7,7 +7,10 @@ const TimerControl = (props) => {
         <div className={classes.TimerControl}>
             {!props.started
                 ? props.starting
-                    ? <button className={[classes.btn, classes.success].join(' ')} onClick={props.start}>RESUME</button>
+                    ? <>
+                        <button className={[classes.btn, classes.success].join(' ')} onClick={props.start}>RESUME</button>
+                        <button className={[classes.btn, classes.warning].join(' ')} onClick={props.reset}>RESET</button>
+                    </>
                     : <button className={[classes.btn, classes.success].join(' ')} onClick={props.start}>START</button>
                 : <>
                     <button className={[classes.btn, classes.danger].join(' ')} onClick={props.stop}>STOP</button>
